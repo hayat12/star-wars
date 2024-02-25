dedb52b04f4a4bd2769ecdd46fffee977753a942
hayat <hayat@freightmark.com.my>
2024-02-25 16:56:16 +0100

    films mock test data

--- src/app/movies/home/home.component.spec.ts
+++ src/app/movies/home/home.component.spec.ts
@@ -2,22 +2,47 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { StoreModule } from '@ngrx/store';
+import { MoviesService } from '../services/movies.service';
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
+import { Pagination } from 'src/app/shared/interfaces/pagination.interface';
+import { FilmsInterface } from '../interfaces/films.interface';
+import { FILMS } from 'src/app/mock/films';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-      imports: [StoreModule.forRoot({})]
+    let service: MoviesService;
+    let httpTestingController: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HomeComponent],
+            imports: [StoreModule.forRoot({}), HttpClientTestingModule],
+            providers: [MoviesService],
+        });
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        httpTestingController = TestBed.inject(HttpTestingController);
+        service = TestBed.inject(MoviesService);
     });
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get movies', () => {
+        service.getFilms().subscribe((data)=>{
+            expect(data).toBeTruthy();
+        })
+
+        const mockRequest = httpTestingController.expectOne(`${service.endPoint}/films`);
+        expect(mockRequest.request.method).toBe("GET");
+        mockRequest.flush(Object.values(FILMS))
+    });
 });
