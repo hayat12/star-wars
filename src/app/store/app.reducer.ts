4c07e8313e6d16d532a87883db351733c52a80a0
hayat <hayat@freightmark.com.my>
2024-02-26 13:30:27 +0100

    app state updated

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,19 +2,29 @@ import { createFeature, createReducer, on } from '@ngrx/store';
 import { AppState, Success, appActions } from './app.actions';
 
 const initialState: AppState<Success | Error> = {
-  loading: false,
-  app: null
+    loading: false,
+    app: null,
 };
 const loadAppReducer = createFeature({
-  name: 'app',
-  reducer: createReducer(
-    initialState,
-    on(appActions.request, (state) => (
-        { ...state, loading: true })),
-    on(appActions.appSuccess, (state) => (
-        { ...state, loading: false })),
-    on(appActions.appError, (state) => ({ ...state, loading: false })),
-)});
+    name: 'app',
+    reducer: createReducer(
+        initialState,
+        on(appActions.request, (state) => ({ ...state, loading: true })),
+        on(appActions.appSuccess, (state, actions) => ({
+            ...state,
+            loading: false,
+            app: actions.success
+        })),
+        on(appActions.appError, (state, actions) => ({
+            ...state,
+            loading: false,
+            app: actions.error
+        }))
+    ),
+});
 export const {
-  name: characterKeyFeature,
-  reducer: appReducer, selectApp, selectLoading } = loadAppReducer;
+    name: characterKeyFeature,
+    reducer: appReducer,
+    selectApp,
+    selectLoading,
+} = loadAppReducer;
